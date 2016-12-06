8faac421105c8a5445b2c350bc6f3b8d74245461
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-06 14:34:23 -0500

    add Devise

--- messages/app/models/user.rb
+++ messages/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   include ActiveModel::Serializers::JSON
   has_many :messages, through: :message_senders, :foreign_key => 'sender_id'
   has_many :messages, through: :message_recipients, :foreign_key => 'recipient_id'
