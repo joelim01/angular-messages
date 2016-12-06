1ffd3418a6d885e43b3d21d168dcf1b7f53f508f
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-06 14:34:59 -0500

    Navbar and NavCtrl

---
+++ messages/app/assets/javascripts/nav/navCtrl.js
@@ -0,0 +1,25 @@
+var NavCtrl = function($scope, Auth) {
+    $scope.signedIn = Auth.isAuthenticated;
+    $scope.logout = Auth.logout;
+
+    Auth.currentUser().then(function (user){
+        $scope.user = user;
+    });
+
+    $scope.$on('devise:new-registration', function (e, user){
+        $scope.user = user;
+    });
+
+    $scope.$on('devise:login', function (e, user){
+        $scope.user = user;
+    });
+
+    $scope.$on('devise:logout', function (e, user){
+        $scope.user = {};
+    });
+}
+
+NavCtrl.$inject = ['$scope', 'Auth'];
+
+angular.module('myApp')
+    .controller('NavCtrl', NavCtrl)
