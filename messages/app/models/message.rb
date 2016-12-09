cd931082a2711bbe0c796b8461e41cd04d9964d1
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-09 12:56:03 -0500

    Misc

--- messages/app/models/message.rb
+++ messages/app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ApplicationRecord
-  before_filter :authenticate_user!, only: [:create]
   include ActiveModel::Serializers::JSON
   has_many :senders, :class_name => "User", through: :message_senders
   has_many :recipients, :class_name => "User", through: :message_recipients
