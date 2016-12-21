458135f7f321233764ee7f4f58450af6b06e40f0
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-21 11:43:28 -0500

    Message Controller refactor and validation

--- messages/app/assets/javascripts/messages/messageCtrl.js
+++ messages/app/assets/javascripts/messages/messageCtrl.js
@@ -1,21 +1,49 @@
-var MessageCtrl = function($rootScope, state, MessageService, $log, datepickerService, $http) {
+var MessageCtrl = function($rootScope, state, MessageService, $log, datepickerService, $http, Flash, $filter) {
   var MC = this;
 
   MessageService.getMessages();
-  MC.message = MessageService.newMessage;
+  MC.message = angular.copy(MessageService.newMessage);
   MC.getUsers = MessageService.getUsers;
   MC.submitMessage = submitMessage;
   MC.messages = MessageService.messages;
+  MC.sentMsg = "Message sent!";
 
-  function submitMessage() {
-    Object.assign(MC.message, datepickerService.getDates())
-    var data = {message: MC.message}
-    MessageService.submit(data, $rootScope.currentUser);
+  function submitMessage(form) {
+    addDateToMessage();
+    if (validateDates()) {
+      var data = {message: MC.message}
+      MessageService.submit(data, $rootScope.currentUser).then(function() {
+        Flash.create('success', ("The message will be sent between " + $filter('date')(data.message.dt, "dd/MM/yyyy") + " and " + $filter('date')(data.message.dt2, "dd/MM/yyyy")+"."));
+        resetForm();
+      }).catch(function() {
+        Flash.create('error', "There has been an error.");
+      })
+    }
   };
 
-}
+  function addDateToMessage() {
+    Object.assign(MC.message, datepickerService.getDates())
+  }
 
-MessageCtrl.$inject = ['$rootScope', '$state', 'MessageService', '$log', 'datepickerService', '$http'];
+  function validateDates() {
+    if (MC.message.dt > MC.message.dt2) {
+      form.$error.dateError = true;
+      return false;
+    }
+    return true;
+  }
+
+  function resetForm() {
+    form.$setUntouched();
+    form.$setPristine();
+    Flash.create('success', MC.sentMsg);
+    MC.message = angular.copy(MessageService.newMessage);
+  }
+  
+};
+
+
+MessageCtrl.$inject = ['$rootScope', '$state', 'MessageService', '$log', 'datepickerService', '$http', 'Flash', '$filter'];
 
 angular.module('myApp')
     .controller('MessageCtrl', MessageCtrl);
