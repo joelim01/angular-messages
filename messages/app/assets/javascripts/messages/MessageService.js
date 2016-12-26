f9223df0c05959ba29f21c3256663b31686b8fa3
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-26 13:11:04 -0500

    MessageService updates

--- messages/app/assets/javascripts/messages/MessageService.js
+++ messages/app/assets/javascripts/messages/MessageService.js
@@ -1,7 +1,7 @@
 function MessageService($rootScope, $http, Auth, Flash) {
   var MS = this;
 
-  this.newMessage = {
+  MS.newMessage = {
       to: [],
       self: false,
       private: false,
@@ -12,25 +12,60 @@ function MessageService($rootScope, $http, Auth, Flash) {
 
   MS.messages = [];
 
-  MS.submit = function(message, user) {
-    var path = "/api/user/" + user.id + "/messages"
-     return $http.post(path, message)
-            .then(submitMessageComplete)
-            .catch(submitMessageFailed);
-
-      function submitMessageComplete(response) {
-          return response;
-      }
-      function submitMessageFailed(error) {
-          Flash.create('alert', 'XHR Failed for submit.' + error.data);
-      }
+  var submitRequest = function(message, user, path, action) {
+    var jsonMessage = { "message": message }
+    if (action == 'post'){
+        return $http.post(path, jsonMessage)
+           .then(submitRequestComplete)
+           .catch(submitRequestFailed);
+    } else if (action == 'patch') {
+        return $http.patch(path, jsonMessage)
+           .then(submitRequestComplete)
+           .catch(submitRequestFailed);
+    } else if (action == 'destroy') {
+        return $http.delete(path)
+         .then(submitRequestComplete)
+         .catch(submitRequestFailed);
+    }
+      function submitRequestComplete(response) {
+        return response;
+    }
+      function submitRequestFailed(error) {
+        Flash.create('alert', 'XHR Failed for submit.' + error.data);
+    }
   }
 
-  this.getUsers = function(query) {
+  MS.create = function(message, user) {
+    var path = "/api/user/" + user.id + "/messages"
+    return submitRequest(message, user, path, "post")
+  }
+
+  MS.update = function(message, user) {
+    var path = "/api/user/" + user.id + "/messages/" + message.id
+    return submitRequest(message, user, path, "patch")
+  }
+
+  MS.destroy = function(message, user, location) {
+    if (location === "inbox") {
+      var path = "/api/message_recipients/" + message.message_recipients[0].id
+    } else if (location === "outbox") {
+      var path = "/api/user/" + user.id + "/messages/" + message.id
+    }
+    return submitRequest(message, user, path, "destroy")
+  }
+
+  MS.read = function(message) {
+    if (!message.message_recipients[0].read) {
+      message.message_recipients[0].read = true
+      MS.update(message, $rootScope.currentUser);
+    }
+  }
+
+  MS.getUsers = function(query) {
     return $http.get('/api/user/autocomplete?query=' + query)
   }
 
-  this.getMessages = function() {
+  MS.getMessages = function() {
     return Auth.currentUser().then(function(user) {
       var path = '/api/user/' + user.id + '/messages'
       return $http.get(path).then(function(response){
