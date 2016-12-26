3728c3c6e17fd5cd8b1248bb506c1e757f425302
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-26 13:14:23 -0500

    Rb message Model, messageSerializer updates

--- messages/app/models/message.rb
+++ messages/app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ApplicationRecord
   include ActiveModel::Serializers::JSON
-  has_one :message_sender
+  has_one :message_sender, :dependent => :destroy
   has_one :sender, :class_name => "User", through: :message_sender
-  has_many :message_recipients
+  has_many :message_recipients, :dependent => :destroy
   has_many :recipients, :class_name => "User", through: :message_recipients
+
 end
--- messages/app/serializers/message_serializer.rb
+++ messages/app/serializers/message_serializer.rb
@@ -2,4 +2,9 @@ class MessageSerializer < ActiveModel::Serializer
   attributes(:id, :subject, :content, :sent_on)
   has_one :sender, serializer: SenderSerializer
   has_many :recipients, serializer: RecipientSerializer
+  has_many :message_recipients, serializer: MessageRecipientSerializer do
+    object.message_recipients.where(recipient_id: scope.id)
+  end
+
+
 end
