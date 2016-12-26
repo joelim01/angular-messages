ceeb9dd9648552fc2c222b46d14355b4e44227fc
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-26 13:13:56 -0500

    Ruby MessagesController updates

--- messages/app/controllers/messages_controller.rb
+++ messages/app/controllers/messages_controller.rb
@@ -1,15 +1,16 @@
 class MessagesController < ActionController::Base
+  require 'pry'
   include ScheduleSending
   protect_from_forgery with: :exception
   before_filter :authenticate_user!, only: [:create]
-
+  serialization_scope :current_user
 
   def index
     if !params[:user_id]
       messages = Message.all
       render json: messages
     else
-      messages = Message.joins(:message_recipients).joins(:message_sender).where("sender_id = :sender or recipient_id= :recipient", {sender: params[:user_id], recipient: params[:user_id]})
+      messages = Message.joins(:message_recipients).joins(:message_sender).where("sender_id = :sender or recipient_id= :recipient", {sender: params[:user_id], recipient: params[:user_id]}).distinct
       render json: messages
     end
   end
@@ -40,16 +41,23 @@ class MessagesController < ActionController::Base
 
   def update
     message = Message.find(params[:id])
+    if params[:message][:message_recipients]
+      mr = params[:message][:message_recipients][0]
+      MessageRecipient.find(mr[:id]).update(read: mr[:read])
+    end
     if message.update(message_params)
-      message.recipients = []
-      message.recipients << current_user if params[:self]
-      formattedRecipients = params[:message][:to].map(&:values).flatten
+      if params[:message][:to]
+        message.recipients = []
+        formattedRecipients = params[:message][:to].map(&:values).flatten
+      end
       if formattedRecipients.respond_to?(:each)
         formattedRecipients.each { |r| message.recipients << User.where(["username = ?", r]) }
       else
         message.recipients << User.where(["username = ?", formattedRecipients])
       end
-      message.scheduled_send_date = ScheduleSending.call(params[:message][:dt], params[:message][:dt2])
+      if (params[:date_change])
+        message.scheduled_send_date = ScheduleSending.call(params[:message][:dt], params[:message][:dt2])
+      end
       if message.save
         render json: message
       else
@@ -60,10 +68,15 @@ class MessagesController < ActionController::Base
     end
   end
 
-
   private
+
   def message_params
-   params.require(:message).permit(:send_as_group, :content, :private, :subject)
+   params.require(:message).permit(
+   :id,
+   :send_as_group,
+   :content,
+   :private,
+   :subject)
   end
 
 end
