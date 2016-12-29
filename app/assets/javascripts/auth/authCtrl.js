d4a6c5b5d5ed730e3e9705bc804543ecfe6ee5de
Joseph Lim <joseph.d.lim@gmail.com>
2016-12-29 14:39:02 -0500

    Updated inbox and inbox accordion template

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -1,20 +1,29 @@
-var AuthCtrl = function($scope, $state, Auth) {
+var AuthCtrl = function($scope, $state, Auth, Flash) {
   var vm = this;
 
     vm.login = function() {
-        Auth.login(vm.user).then(function(){
-            $state.go('messages');
-        });
+        Auth.login(vm.user)
+          .then(function(){
+            $state.go('messages.compose')})
+          .catch(function(data) {
+            debugger
+            Flash.create('alert', data.data.error);
+          });
     };
 
     vm.register = function() {
-        Auth.register(vm.user).then(function(){
-            $state.go('messages');
-        });
+        Auth.register(vm.user)
+          .then(function(){
+            $state.go('messages.compose')})
+          .catch(function(data) {
+            debugger
+            Flash.create('alert', data.data.error);
+          });
     };
-};
 
-AuthCtrl.$inject = ['$scope', '$state', 'Auth'];
+}
+
+AuthCtrl.$inject = ['$scope', '$state', 'Auth', 'Flash'];
 
 angular.module('myApp')
     .controller('AuthCtrl', AuthCtrl);
