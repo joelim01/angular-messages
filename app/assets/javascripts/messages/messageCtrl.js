6ad2a5839802fcddd13f9069300ae674f5901086
Joseph Lim <joseph.d.lim@gmail.com>
2017-01-10 17:16:59 -0500

    Updated MessageCtrl for new API data structure, service arch

--- app/assets/javascripts/messages/messageCtrl.js
+++ app/assets/javascripts/messages/messageCtrl.js
@@ -1,19 +1,34 @@
-var MessageCtrl = function($rootScope, $state, MessageService, $log, datepickerService, $http, Flash, $filter, MessageFormService) {
+var MessageCtrl = function($rootScope, $state, MessageService, $log, DatepickerService, $http, Flash, $filter, MessageFormService, PaginationService) {
   var MC = this;
+  var PS = PaginationService;
+  var MS = MessageService;
 
-  MessageService.getMessages();
+  MC.filterUnread = false;
+  MC.message = angular.copy(MessageService.newMessage);
+  MC.mergeDates = mergeDates;
+  MC.messages = {};
+
+  MC.createMessage = createMessage;
   MC.deleteFromInbox = deleteInboxMessage;
   MC.deleteFromOutbox = deleteOutboxMessage;
   MC.getUsers = MessageService.getUsers;
-  MC.filterUnread = false;
-  MC.message = angular.copy(MessageService.newMessage);
-  MC.messages = MessageService.messages;
-  MC.submitMessage = submitMessage;
   MC.read = MessageService.read;
   MC.unread = unread;
-  MC.validateDates = MessageFormService.validateDates
+  MC.validateForm = MessageFormService.validateForm
 
-  function unread() {
+  activate();
+
+  function activate() {
+    MessageService.getMessages(PS.pages.inboxPage, "inbox").then(function(response) {
+      MessageService.getMessages(PS.pages.outboxPage, "outbox").then(function(response) {
+        MessageService.getMessages(PS.pages.sentPage, "sent").then(function(response) {
+          MC.messages = { data: MS.messages }
+        })
+      })
+    })
+  }
+
+  function unread(message) {
     return function(message) {
       if (MC.filterUnread == true) {
         return message.message_recipients[0].read == false
@@ -24,25 +39,34 @@ var MessageCtrl = function($rootScope, $state, MessageService, $log, datepickerS
   }
 
   function deleteInboxMessage(message) {
-    MessageService.destroy(message, $rootScope.currentUser, "inbox").then(function () {
+    MessageService.destroyMessage(message, "inbox").then(function () {
       mArray = [message]
-      MC.messages = $(MC.messages).not(mArray).get();
+      MC.messages.inboxMessages = $(MC.messages.inboxMessages).not(mArray).get();
     })
   }
 
   function deleteOutboxMessage(message) {
-    MessageService.destroy(message, $rootScope.currentUser, "outbox").then(function () {
+    MessageService.destroyMessage(message, "outbox").then(function () {
       mArray = [message]
-      MC.messages = $(MC.messages).not(mArray).get();
+      MC.messages.outboxMessages = $(MC.messages.outboxMessages).not(mArray).get();
     })
   }
 
-  function submitMessage(form) {
-    addDateToMessage();
-    if (MC.validateDates(form, MC.message)) {
-      (function (form) { MessageService.create(MC.message, $rootScope.currentUser).then(function(data) {
-          Flash.create('success', ("The message will be sent between " + $filter('date')(MC.message.dt, "dd/MM/yyyy") + " and " + $filter('date')(MC.message.dt2, "dd/MM/yyyy")+"."));
-          resetForm(form);
+  function resetForm(form) {
+    form.$setPristine()
+    form.$setUntouched()
+    MC.message = angular.copy(MessageService.newMessage);
+    DatepickerService.resetDates();
+    console.log(DatepickerService)
+  }
+
+  function createMessage(form) {
+    mergeDates(MC.message)
+    if (MC.validateForm(form, MC.message)) {
+      (function (form) { MessageService.createMessage(MC.message).then(function(data) {
+        debugger
+          Flash.create('success', ("The message will be sent between " + $filter('date')(MC.message.dt, "MM/dd/yyyy") + " and " + $filter('date')(MC.message.dt2, "MM/dd/yyyy")+"."));
+          resetForm(form)
         }).catch(function(response) {
           Flash.create('error', "There has been an error.");
         });
@@ -50,23 +74,14 @@ var MessageCtrl = function($rootScope, $state, MessageService, $log, datepickerS
     }
   };
 
-  function addDateToMessage() {
-    Object.assign(MC.message, datepickerService.getDates())
-  }
-
-  function resetForm(form) {
-    MessageFormService.resetForm(form);
-    MC.message = angular.copy(MessageService.newMessage);
-  }
-
-  function resetMessage() {
-    MC.message = angular.copy(MessageService.newMessage);
+  function mergeDates(message) {
+    MC.message.dt = DatepickerService.dates.dt
+    MC.message.dt2 = DatepickerService.dates.dt2
   }
 
 };
 
-
-MessageCtrl.$inject = ['$rootScope', '$state', 'MessageService', '$log', 'datepickerService', '$http', 'Flash', '$filter', 'MessageFormService'];
+MessageCtrl.$inject = ['$rootScope', '$state', 'MessageService', '$log', 'DatepickerService', '$http', 'Flash', '$filter', 'MessageFormService', 'PaginationService'];
 
 angular.module('myApp')
     .controller('MessageCtrl', MessageCtrl);
