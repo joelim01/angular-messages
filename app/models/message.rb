3bca7f5ad959af0604b9c750705fd3c392aecd76
Joseph Lim <joseph.d.lim@gmail.com>
2017-01-10 17:17:52 -0500

    Nested attributes on Message API

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
   has_one :sender, :class_name => "User", through: :message_sender
   has_many :message_recipients, :dependent => :destroy
   has_many :recipients, :class_name => "User", through: :message_recipients
+  accepts_nested_attributes_for :recipients
+  accepts_nested_attributes_for :message_recipients
 end
